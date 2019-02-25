cd1ef5f1d0f7f8acd973e3c0fe29799589215f08
Li Beibei <libeibei@kelvins.io>
2019-02-25 20:51:42 +0800

    use idea to init

---
+++ src/com/robby/al100/chp1/MyStack1.java
@@ -0,0 +1,25 @@
+package com.robby.al100.chp1;
+
+import java.util.Stack;
+/**
+ * @author: Robby
+ */
+
+public class MyStack1 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public Mystack1(){
+        this.stackData = new Stack<Integer>();
+        this.stackMin = new Stack<Integer>();
+    }
+
+    public void push(int newNum) {
+        if (this.stackMin.isEmpty())
+    }
+
+    public static void main(String[] args) {
+        System.out.println("chp1");
+    }
+
+}
--- src/main/java/com/robby/al100/chp1/1.1.java
+++
@@ -1,5 +0,0 @@
-package com.robby.al100;
-
-
-
-
