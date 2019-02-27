e500e0c3fceff6f36d7ddbde7307e17481be3205
Li Beibei <libeibei@kelvins.io>
2019-02-27 21:48:52 +0800

    problem 1.3

---
+++ src/com/robby/al100/chp1/ReversedStack.java
@@ -0,0 +1,26 @@
+package com.robby.al100.chp1;
+
+import java.util.Stack;
+
+public class ReversedStack {
+    public static int getAndRemoveLastElement(Stack<Integer> stack) {
+        int result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        } else {
+            int last = getAndRemoveLastElement(stack);
+            stack.push(result);
+            return getAndRemoveLastElement(stack);
+        }
+    }
+
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int i = getAndRemoveLastElement(stack);
+        reverse(stack);
+        stack.push(i);
+    }
+
+}
