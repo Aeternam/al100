37fd32d674fe0356c936112beece16abcf0fa3a5
Li Beibei <libeibei@kelvins.io>
2019-03-01 18:32:31 +0800

    problem 1.7

---
+++ src/com/robby/al100/chp1/MaxWindowArray.java
@@ -0,0 +1,28 @@
+package com.robby.al100.chp1;
+
+import java.util.LinkedList;
+
+public class MaxWindowArray {
+    public int[] getMaxWindow(int[] arr, int w) {
+        if (arr == null || w < 1 || arr.length < w) {
+            return null;
+        }
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int[] res = new int[arr.length - w + 1];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            if (qmax.peekFirst() == i - w) {
+                qmax.pollFirst();
+            }
+            if (i >= w - 1) {
+                res[index++] = arr[qmax.peekFirst()];
+            }
+        }
+        return res;
+    }
+
+}
