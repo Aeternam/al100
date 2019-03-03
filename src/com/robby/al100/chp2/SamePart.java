3ba3d0721a95b6dd08c500079d862ae4f330c5dc
Robby <justfan.b@gmail.com>
2019-03-03 14:28:25 +0000

    problem 2.1

---
+++ src/com/robby/al100/chp2/SamePart.java
@@ -0,0 +1,26 @@
+package com.robby.al100.chp2;
+
+
+public class Node {
+    public int value;
+    public Nde next;
+    public Node(int data) {
+        this.value = data;
+    }
+
+    public void printCommonPart(Node head1, Node head2) {
+        System.out.print("Common Part: ");
+        while (head1 != null && head2 != null) {
+            if (head1.value < head2.value) {
+                head1 = head1.next;
+            } else if (head1.value > head2.value) {
+                head2 = head2.next;
+            } else {
+                System.out.print(head1.value + " ");
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        System.out.println();
+    }
+}
