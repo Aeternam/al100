72a4da54cc985f2d57b8926c46d08833585cdcc0
Li Beibei <libeibei@kelvins.io>
2019-03-04 17:31:16 +0800

    problem 2.2

--- src/com/robby/al100/chp2/Node.java
+++ src/com/robby/al100/chp2/Node.java
@@ -23,4 +23,26 @@ public class Node {
         }
         System.out.println();
     }
+
+    public Node removeLastKthNode(Node head, int lastKth) {
+        if (head ==null || lastKth < 1) {
+            return head;
+        }
+        Node cur = head;
+        while (cur != null) {
+            lastKth--;
+            cur = cur.next;
+        }
+        if (lastKth == 0) {
+            head = head.next;
+        }
+        if (lastKth < 0) {
+            cur = head;
+            while (++lastKth != 0) {
+                cur = cur.next;
+            }
+            cur.next = cur.next.next;
+        }
+        return head;
+    }
 }
